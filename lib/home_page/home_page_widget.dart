1769a7d30739aebcafe6a78ba9d75da0251e5bd7
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-10-07 07:28:00 +0000

    Updating to latest FlutterFlow output.

--- lib/home_page/home_page_widget.dart
+++ lib/home_page/home_page_widget.dart
@@ -1,5 +1,3 @@
-import '/backend/gemini/gemini.dart';
-import '/components/answer_view_widget.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
@@ -7,6 +5,7 @@ import '/flutter_flow/upload_data.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
 import 'home_page_model.dart';
 export 'home_page_model.dart';
@@ -38,7 +37,7 @@ class _HomePageWidgetState extends State<HomePageWidget> {
       if (selectedMedia != null &&
           selectedMedia
               .every((m) => validateFileFormat(m.storagePath, context))) {
-        safeSetState(() => _model.isDataUploading = true);
+        safeSetState(() => _model.isDataUploading1 = true);
         var selectedUploadedFiles = <FFUploadedFile>[];
 
         try {
@@ -52,11 +51,11 @@ class _HomePageWidgetState extends State<HomePageWidget> {
                   ))
               .toList();
         } finally {
-          _model.isDataUploading = false;
+          _model.isDataUploading1 = false;
         }
         if (selectedUploadedFiles.length == selectedMedia.length) {
           safeSetState(() {
-            _model.uploadedLocalFile = selectedUploadedFiles.first;
+            _model.uploadedLocalFile1 = selectedUploadedFiles.first;
           });
         } else {
           safeSetState(() {});
@@ -64,38 +63,20 @@ class _HomePageWidgetState extends State<HomePageWidget> {
         }
       }
 
-      if (_model.uploadedLocalFile != null &&
-          (_model.uploadedLocalFile.bytes?.isNotEmpty ?? false)) {
-        await geminiTextFromImage(
+      if (_model.uploadedLocalFile1 != null &&
+          (_model.uploadedLocalFile1.bytes?.isNotEmpty ?? false)) {
+        _model.isLoading = true;
+        safeSetState(() {});
+        await _model.getData(
           context,
-          'ช่วยบอกหน่อยว่ารูปภาพนี้คืออะไรและอธบายรายเอียดเล็กน้อยด้วย',
-          uploadImageBytes: _model.uploadedLocalFile,
-        ).then((generatedText) {
-          safeSetState(() => _model.aiResult = generatedText);
-        });
-
-        await showModalBottomSheet(
-          isScrollControlled: true,
-          backgroundColor: Colors.transparent,
-          enableDrag: false,
-          useSafeArea: true,
-          context: context,
-          builder: (context) {
-            return GestureDetector(
-              onTap: () => FocusScope.of(context).unfocus(),
-              child: Padding(
-                padding: MediaQuery.viewInsetsOf(context),
-                child: AnswerViewWidget(
-                  answerParameter: _model.aiResult!,
-                ),
-              ),
-            );
-          },
-        ).then((value) => safeSetState(() {}));
+          image: _model.uploadedLocalFile1,
+        );
+        _model.isLoading = false;
+        safeSetState(() {});
       }
       safeSetState(() {
-        _model.isDataUploading = false;
-        _model.uploadedLocalFile =
+        _model.isDataUploading1 = false;
+        _model.uploadedLocalFile1 =
             FFUploadedFile(bytes: Uint8List.fromList([]));
       });
     });
@@ -117,9 +98,124 @@ class _HomePageWidgetState extends State<HomePageWidget> {
         backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
         body: SafeArea(
           top: true,
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: [],
+          child: Stack(
+            children: [
+              if (!_model.isLoading)
+                Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Align(
+                      alignment: AlignmentDirectional(0.0, 0.0),
+                      child: InkWell(
+                        splashColor: Colors.transparent,
+                        focusColor: Colors.transparent,
+                        hoverColor: Colors.transparent,
+                        highlightColor: Colors.transparent,
+                        onTap: () async {
+                          final selectedMedia = await selectMedia(
+                            maxWidth: 600.00,
+                            imageQuality: 100,
+                            multiImage: false,
+                          );
+                          if (selectedMedia != null &&
+                              selectedMedia.every((m) =>
+                                  validateFileFormat(m.storagePath, context))) {
+                            safeSetState(() => _model.isDataUploading2 = true);
+                            var selectedUploadedFiles = <FFUploadedFile>[];
+
+                            try {
+                              selectedUploadedFiles = selectedMedia
+                                  .map((m) => FFUploadedFile(
+                                        name: m.storagePath.split('/').last,
+                                        bytes: m.bytes,
+                                        height: m.dimensions?.height,
+                                        width: m.dimensions?.width,
+                                        blurHash: m.blurHash,
+                                      ))
+                                  .toList();
+                            } finally {
+                              _model.isDataUploading2 = false;
+                            }
+                            if (selectedUploadedFiles.length ==
+                                selectedMedia.length) {
+                              safeSetState(() {
+                                _model.uploadedLocalFile2 =
+                                    selectedUploadedFiles.first;
+                              });
+                            } else {
+                              safeSetState(() {});
+                              return;
+                            }
+                          }
+
+                          if (_model.uploadedLocalFile2 != null &&
+                              (_model.uploadedLocalFile2.bytes?.isNotEmpty ??
+                                  false)) {
+                            _model.isLoading = true;
+                            safeSetState(() {});
+                            await _model.getData(
+                              context,
+                              image: _model.uploadedLocalFile2,
+                            );
+                            _model.isLoading = false;
+                            safeSetState(() {});
+                          }
+                          safeSetState(() {
+                            _model.isDataUploading2 = false;
+                            _model.uploadedLocalFile2 =
+                                FFUploadedFile(bytes: Uint8List.fromList([]));
+                          });
+                        },
+                        child: Material(
+                          color: Colors.transparent,
+                          elevation: 3.0,
+                          shape: const CircleBorder(),
+                          child: Container(
+                            width: 100.0,
+                            height: 100.0,
+                            decoration: BoxDecoration(
+                              color: FlutterFlowTheme.of(context)
+                                  .secondaryBackground,
+                              shape: BoxShape.circle,
+                            ),
+                            child: Padding(
+                              padding: EdgeInsets.all(4.0),
+                              child: Container(
+                                width: double.infinity,
+                                height: double.infinity,
+                                clipBehavior: Clip.antiAlias,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                ),
+                                child: Image.asset(
+                                  'assets/images/question-mark_4412442.png',
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              if (_model.isLoading)
+                Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Color(0x59000000),
+                  ),
+                  child: Lottie.asset(
+                    'assets/lottie_animations/Animation_-_1728285980280.json',
+                    width: 200.0,
+                    height: 200.0,
+                    fit: BoxFit.contain,
+                    animate: true,
+                  ),
+                ),
+            ],
           ),
         ),
       ),
